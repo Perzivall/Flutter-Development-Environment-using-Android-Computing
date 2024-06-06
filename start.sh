5b122cfc4f4225866b5954d2420b71704c2be05f
Perzival <87344717+Perzivall@users.noreply.github.com>
2024-06-06 01:59:21 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,28 @@
-#Script for discovery IP Address and configure
-ip_address=$(ifconfig | grep 'inet' | awk '{print $2}' | grep '192')
+#!/bin/bash
+
+# Obtendo o endereço IP
+ip_address=$(ifconfig | grep 'inet ' | awk '{print $2}' | grep '192' | head -n 1)
+
+# Caminho do arquivo de configuração
 file_path="/root/.config/code-server/config.yaml"
 
-if [ $ip_address == "192.0.0.8" ]; then
-        echo "Não foi possivel obter IP, usando localhost"
-        ip_address="127.0.0.1"
+# Verificando se a variável ip_address está vazia
+if [ -z "$ip_address" ]; then
+    echo "Não foi possível obter IP, usando localhost"
+    ip_address="127.0.0.1"
 fi
-                                                               
-sed -i -E "s/^(bind-addr:).*/\1 $ip_address:8080/" "$file_path"
+
+# Usando sed para substituir tudo após o ':' em linhas que contêm 'bind-addr'
+# Utilizando | como delimitador para evitar conflitos com caracteres IP
+sed -i -E "s|^(bind-addr:).*|\1 $ip_address:8080|" "$file_path"
+
+# Mensagem de ativação do servidor
 echo "-----------------------------------------"
 echo "Ativando servidor"
 
-nohup code-server & disown 
-
-echo "Acesse o ip $ip_address:8080"
+# Iniciando o code-server em segundo plano e desanexando do terminal
+nohup code-server & disown
+                  
+# Informando o IP de acesso
+echo "Acesse o IP http://$ip_address:8080"
 echo "-----------------------------------------"
