08c5229e4263e6bbe27b323b9308a702edc797e6
Perzivall <87344717+Perzivall@users.noreply.github.com>
2024-06-21 16:51:08 -0300

    Create auto_install_step_2.sh

---
+++ auto_install_step_2.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+apt update -y && apt upgrade -y && 
+apt install sudo unzip xz-utils zip libglu1-mesa libc6:arm64 libncurses5:arm64 libstdc++6:arm64 libbz2-1.0:arm64 clang cmake git ninja-build pkg-config libgtk-3-dev liblzma-dev libstdc++-12-dev -y &&
+
+#DADOS DO USUARIO
+read -p "Crie um novo usuario: " YOUR_USER 
+read -sp "Digite uma senha: " YOUR_PASSWORD 
+
+#
+echo '$YOUR_USER ALL=(ALL:ALL) ALL' | sudo tee -a /etc/sudoers
+useradd -m -s /bin/bash $YOUR_USER && echo "$YOUR_USER:$YOUR_PASSWORD" | chpasswd && usermod -aG sudo $YOUR_USER
+
+su $YOUR_USER
+cd $HOME
+curl -fsSL https://code-server.dev/install.sh | sh
+wget https://github.com/Perzivall/Flutter-Server-Android-arm64/releases/download/34.0.4/start.sh
+wget https://github.com/Perzivall/Flutter-Web-Development-Environment-using-Android/releases/download/34.0.4/configure_password.sh
+chmod +x start.sh
+chmod +x configure_password.sh
+file_path="/home/$(whoami)/.config/code-server/config.yaml"
+
+sed -i -E "s|^(password:).*|\1 $YOUR_PASSWORD|" "$file_path"
+
+./configure_password.sh
+mkdir /home/$(whoami)/android-sdk
+cd /home/$(whoami)/android-sdk
+wget https://github.com/Perzivall/Flutter-Server-Android-arm64/releases/download/34.0.4/build-tools-34.0.4-aarch64.tar.xz 
+wget https://github.com/Perzivall/Flutter-Server-Android-arm64/releases/download/34.0.4/platform-tools-34.0.4-aarch64.tar.xz 
+wget https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip 
+tar -xf build-tools-34.0.4-aarch64.tar.xz 
+tar -xf platform-tools-34.0.4-aarch64.tar.xz 
+unzip commandlinetools-linux-11076708_latest.zip 
+cd cmdline-tools 
+mkdir latest 
+mv * latest 
+cd $HOME
+cd $HOME
+cd $HOME/android-sdk
+git clone https://github.com/flutter/flutter.git
+cd $HOME/android-sdk/flutter/bin
+./flutter doctor -v
+echo 'export ANDROID_SDK_ROOT="$HOME/android-sdk/"' >> ~/.bashrc
+echo 'export ANDROID_HOME="$HOME/android-sdk/"' >> ~/.bashrc
+echo 'export PATH="$PATH:$HOME/android-sdk/build-tools/34.0.4"' >> ~/.bashrc
+echo 'export PATH="$PATH:$HOME/android-sdk/platform-tools"' >> ~/.bashrc
+echo 'export PATH="$PATH:$HOME/android-sdk/flutter/bin"' >> ~/.bashrc
+echo 'export PATH="$PATH:$HOME/android-sdk/cmdline-tools/latest/bin"' >> ~/.bashrc
+source ~/.bashrc
