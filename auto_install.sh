3c4f391d7119115e883e8b87a1962d6614838dac
Perzivall <87344717+Perzivall@users.noreply.github.com>
2024-06-21 17:23:09 -0300

    Update auto_install.sh

--- auto_install.sh
+++ auto_install.sh
@@ -1,10 +1,10 @@
 #!bin/bash
 
-pkg update -y &&
-pkg upgrade -y &&
-pkg install proot-distro pkg-config openjdk-17 wget git vim curl-y &&
-proot-distro install debian &&
-termux-wake-lock && 
-proot-distro login debian -- ; wget https://raw.githubusercontent.com/Perzivall/Flutter-Web-Development-Environment-using-Android/main/auto_install_step_2.sh ; ./auto_install_step_2.sh
+pkg update -y 
+pkg upgrade -y 
+pkg install proot-distro pkg-config openjdk-17 wget git vim curl -y 
+proot-distro install debian 
+termux-wake-lock
+proot-distro login debian -- wget https://raw.githubusercontent.com/Perzivall/Flutter-Web-Development-Environment-using-Android/main/auto_install_step_2.sh && chmod +x auto_install_step_2.sh && ./auto_install_step_2.sh
 
 echo "Instalação concluida!"
