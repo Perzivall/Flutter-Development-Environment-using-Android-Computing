37dd3d16f4f35389dd6f88263d64de727fc44e53
Perzivall <87344717+Perzivall@users.noreply.github.com>
2024-06-22 17:06:18 -0300

    Atualizar o auto_install.sh

--- auto_install.sh
+++ auto_install.sh
@@ -23,7 +23,7 @@ read -p 'Enter a user: ' YOUR_USER
 echo
 read -p 'Enter a password: ' YOUR_PASSWORD
 echo
-echo 'proot-distro login debian --user $YOUR_USER -- bash -c "./start.sh && /bin/bash' >> start.sh
+echo 'proot-distro login debian --user $YOUR_USER -- bash -c "./start.sh && /bin/bash"' >> start.sh
 echo
 echo 'WARNING!'
 echo 'Whenever you want to start Enviroment, type ./start.sh'
