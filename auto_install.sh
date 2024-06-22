4f8ab9158de9d25ee45b87e447cd053cdd3e2536
Perzivall <87344717+Perzivall@users.noreply.github.com>
2024-06-22 09:15:37 -0300

    Update auto_install.sh

--- auto_install.sh
+++ auto_install.sh
@@ -2,10 +2,10 @@
 
 pkg update -y 
 pkg upgrade -y 
-pkg install proot-distro -y 
+pkg install proot-distro wget -y 
 proot-distro install debian 
 termux-wake-lock
-proot-distro login debian -- curl -fsSL https://raw.githubusercontent.com/Perzivall/Flutter-Web-Development-Environment-using-Android/main/auto_install_step_2.sh | sh
+proot-distro login debian -- wget https://raw.githubusercontent.com/Perzivall/Flutter-Web-Development-Environment-using-Android/main/auto_install_step_2.sh
 proot-distro login debian -- chmod +x auto_install_step_2.sh
 proot-distro login debian -- ./auto_install_step_2.sh
 
