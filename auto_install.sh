7486b7fab3924c1e4eca5b0d9086d3d80b005385
Perzivall <87344717+Perzivall@users.noreply.github.com>
2024-06-22 14:58:22 -0300

    Atualizar o auto_install.sh

--- auto_install.sh
+++ auto_install.sh
@@ -1,10 +1,22 @@
-#!bin/bash
+#!/bin/bash
 
-pkg update -y 
-pkg upgrade -y 
-pkg install proot-distro wget ncurses-utils -y 
-proot-distro install debian 
+# Atualizar e instalar pacotes necessários no Termux
+pkg update -y
+pkg upgrade -y
+pkg install proot-distro wget ncurses-utils -y
+
+# Instalar Debian no proot
+proot-distro install debian
+
+# Ativar o wake lock do Termux
 termux-wake-lock
-proot-distro login debian --isolated -- sh -c "apt update -y && apt upgrade -y && apt install curl -y && curl -fsSl https://raw.githubusercontent.com/Perzivall/Flutter-Web-Development-Environment-using-Android/main/auto_install_step_2.sh | bash"
 
-echo "Instalação concluida!"
+# Entrar no ambiente Debian e executar o segundo script
+proot-distro login debian --isolated -- sh -c "
+  apt update -y && apt upgrade -y && apt install curl -y
+  curl -fsSL https://raw.githubusercontent.com/Perzivall/Flutter-Web-Development-Environment-using-Android/main/auto_install_step_2.sh -o /root/auto_install_step_2.sh
+  chmod +x /root/auto_install_step_2.sh
+  bash /root/auto_install_step_2.sh
+"
+
+echo "Instalação concluída!"
