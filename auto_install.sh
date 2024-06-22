cf101a3ef482d9921d74dc319dfbada584da8155
Perzivall <87344717+Perzivall@users.noreply.github.com>
2024-06-22 20:54:21 -0300

    Atualizar o auto_install.sh

--- auto_install.sh
+++ auto_install.sh
@@ -16,14 +16,4 @@ proot-distro login debian -- bash -c "
   apt update -y && apt upgrade -y && apt install wget -y && cd /root/ &&
   wget https://raw.githubusercontent.com/Perzivall/Flutter-Web-Development-Environment-using-Android/main/auto_install_step_2.sh -O auto_install_step_2.sh &&
   chmod +x auto_install_step_2.sh
-" && (read -p 'Enter a user: ' YOUR_USER
-echo
-read -p 'Enter a password: ' YOUR_PASSWORD
-echo
-echo "proot-distro login debian --user $YOUR_USER -- bash -c './start.sh && /bin/bash'" >> start.sh
-echo
-echo 'WARNING!'
-echo 'Whenever you want to start Enviroment, type ./start.sh'
-echo
-chmod +x start.sh
-)
+" 
