0a37f7c529f4188fe40f9c14cb4e5c02fb2cc6ee
Perzivall <87344717+Perzivall@users.noreply.github.com>
2024-06-22 11:52:02 -0300

    Update auto_install.sh

--- auto_install.sh
+++ auto_install.sh
@@ -5,6 +5,6 @@ pkg upgrade -y
 pkg install proot-distro wget ncurses-utils -y 
 proot-distro install debian 
 termux-wake-lock
-proot-distro login debian --link2symlink -- sh -c "wget https://raw.githubusercontent.com/Perzivall/Flutter-Web-Development-Environment-using-Android/main/auto_install_step_2.sh && chmod +x auto_install_step_2.sh && ./auto_install_step_2.sh"
+proot-distro login debian --isolated -- sh -c "wget https://raw.githubusercontent.com/Perzivall/Flutter-Web-Development-Environment-using-Android/main/auto_install_step_2.sh && chmod +x auto_install_step_2.sh && ./auto_install_step_2.sh"
 
 echo "Instalação concluida!"
