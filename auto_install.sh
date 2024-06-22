ec716c34711749383020fc80afe030b9827b8ffa
Perzivall <87344717+Perzivall@users.noreply.github.com>
2024-06-22 15:44:24 -0300

    Atualizar o auto_install.sh

--- auto_install.sh
+++ auto_install.sh
@@ -15,8 +15,7 @@ termux-wake-lock
 proot-distro login debian --isolated -- bash -c "
   apt update -y && apt upgrade -y && apt install wget -y && cd /root/ &&
   wget https://raw.githubusercontent.com/Perzivall/Flutter-Web-Development-Environment-using-Android/main/auto_install_step_2.sh -O auto_install_step_2.sh &&
-  chmod +x auto_install_step_2.sh
+  chmod +x auto_install_step_2.sh && exit
 "
-exit
 
 proot-distro login debian --isolated -- ./auto_install_step_2.sh
