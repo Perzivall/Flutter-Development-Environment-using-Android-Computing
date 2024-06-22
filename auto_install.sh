b46db49dadbfe94cd2e0125d61c8ac45983e969d
Perzivall <87344717+Perzivall@users.noreply.github.com>
2024-06-22 18:40:49 -0300

    Update auto_install.sh

--- auto_install.sh
+++ auto_install.sh
@@ -12,7 +12,7 @@ proot-distro install debian
 termux-wake-lock
 
 # Entrar no ambiente Debian e executar o segundo script
-proot-distro login debian --isolated -- bash -c "
+proot-distro login debian -- bash -c "
   apt update -y && apt upgrade -y && apt install wget -y && cd /root/ &&
   wget https://raw.githubusercontent.com/Perzivall/Flutter-Web-Development-Environment-using-Android/main/auto_install_step_2.sh -O auto_install_step_2.sh &&
   chmod +x auto_install_step_2.sh
@@ -23,7 +23,7 @@ read -p 'Enter a user: ' YOUR_USER
 echo
 read -p 'Enter a password: ' YOUR_PASSWORD
 echo
-echo "proot-distro login debian --isolated --user $YOUR_USER -- bash -c './start.sh && /bin/bash'" >> start.sh
+echo "proot-distro login debian --user $YOUR_USER -- bash -c './start.sh && /bin/bash'" >> start.sh
 echo
 echo 'WARNING!'
 echo 'Whenever you want to start Enviroment, type ./start.sh'
