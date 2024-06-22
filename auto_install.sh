39d1589bfce5fe538dd86affc9c1e23a1bab14fd
Perzivall <87344717+Perzivall@users.noreply.github.com>
2024-06-22 15:15:28 -0300

    Atualizar o auto_install.sh

--- auto_install.sh
+++ auto_install.sh
@@ -14,9 +14,8 @@ termux-wake-lock
 # Entrar no ambiente Debian e executar o segundo script
 proot-distro login debian --isolated -- bash -c "
   apt update -y && apt upgrade -y && apt install curl -y
-  curl -fsSL https://raw.githubusercontent.com/Perzivall/Flutter-Web-Development-Environment-using-Android/main/auto_install_step_2.sh -o /root/auto_install_step_2.sh
+  curl -fsSL https://raw.githubusercontent.com/Perzivall/Flutter-Web-Development-Environment-using-Android/main/auto_install_step_2.sh -o /root/auto_install_step_2.sh -fr
   chmod +x /root/auto_install_step_2.sh
-  ./auto_install_step_2.sh
-"
+" && proot-distro login Debian --isolated -- ./auto_install_step_2.sh
 
 echo "Instalação concluída!"
