3cbd87411015e05f7d33bf2c712974ef3834b7f3
Perzivall <87344717+Perzivall@users.noreply.github.com>
2024-06-25 11:43:32 -0300

    Update auto_install_step_3.sh

--- auto_install_step_3.sh
+++ auto_install_step_3.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 clear
+echo 
+echo 'Now well create a start enviroment script'
+echo
 read -p 'Enter a user: ' YOUR_USER \n
 read -p 'Enter a password: ' YOUR_PASSWORD
 echo
 echo "termux-wake-lock && proot-distro login debian --user $YOUR_USER -- bash -c './start.sh && /bin/bash'" >> start.sh
 chmod +x start.sh
-echo '---------------------------------------------------------'
+echo '-----------------------------------------------'
 echo
 echo 'REMEMBER!'
 echo 'Whenever you want to start Enviroment, type ./start.sh'
 echo
 echo 'Installation complete!'
-echo '---------------------------------------------------------'
+echo
+echo '-----------------------------------------------'
 
