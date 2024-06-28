b2045b7195b23ba7173fbd33253877f6db08add3
Perzivall <87344717+Perzivall@users.noreply.github.com>
2024-06-28 00:33:41 -0300

    Update auto_install.sh

--- auto_install.sh
+++ auto_install.sh
@@ -11,6 +11,6 @@ proot-distro install ubuntu
 # Entrar no ambiente Debian e executar o segundo script
 proot-distro login ubuntu -- bash -c "
   apt update -y && apt upgrade -y && apt install wget -y && cd /root/ &&
-  wget https://raw.githubusercontent.com/Perzivall/Flutter-Web-Development-Environment-using-Android/main/auto_install_step_2.sh -O auto_install_step_2.sh &&
+  wget https://raw.githubusercontent.com/Perzivall/Flutter-Web-Development-Environment-using-Android/main/auto_install_step_2-v2.sh -O auto_install_step_2-v2.sh &&
   chmod +x auto_install_step_2.sh
 " 
